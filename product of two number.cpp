70e2fa4008c7d7e04687acfaff27f44a9b30f6a0
Harjeev-bit <69383052+Harjeev-bit@users.noreply.github.com>
2020-08-08 15:09:42 +0530

    Add files via upload

---
+++ product of two number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double num1, num2, productoftwonumbers;
+	cout<< "Enter two Numbers: \n";
+	
+	//stores two floating point numbers in variable num1 and num2 respectively
+	
+	cin>>num1>>num2;
+	
+	//performs multiplication and stores the result in variable productoftwonumbers
+	productoftwonumbers= num1*num2;
+	
+	cout <<"product= " <<productoftwonumbers;
+	  
+	  return 0;
+}
